b13519464e5aa0e55c4c0b6103ee032e39a02974
JNMarcos <marcos_joaos@hotmail.com>
2016-09-20 23:30:00 -0300

    Volta da classe Info
    Classe Info passa a conter os dados (valor) de funções de R e opcodes de
    I e J.

---
+++ src/desenvolvimento/Info.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package desenvolvimento;
+
+import java.util.Hashtable;
+
+/**
+ * @author JN
+ *
+ */
+public class Info {
+	Hashtable<String, String> funcoesR = new Hashtable<>();
+	Hashtable<String, String> opcodesI = new Hashtable<>();
+	Hashtable<String, String> opcodesJ = new Hashtable<>();
+	
+	Info(){
+		//adição das funções tipos R
+		funcoesR.put("100000", "add");
+		funcoesR.put("100010", "sub");
+		funcoesR.put("101010", "slt");
+		funcoesR.put("100100", "and");
+		funcoesR.put("100101", "or");
+		funcoesR.put("100110", "xor");
+		funcoesR.put("100111", "nor");
+		funcoesR.put("010000", "mfhi");
+		funcoesR.put("010010", "mflo");
+		funcoesR.put("100001", "addu");
+		funcoesR.put("100011", "subu");
+		funcoesR.put("011000", "mult");
+		funcoesR.put("011001", "multu");
+		funcoesR.put("011010", "div");
+		funcoesR.put("011011", "divu");
+		funcoesR.put("000000", "sll");
+		funcoesR.put("000010", "srl");
+		funcoesR.put("000011", "sra");
+		funcoesR.put("000100", "sllv");
+		funcoesR.put("000110", "srlv");
+		funcoesR.put("000111", "srav");
+		funcoesR.put("001000", "jr");
+		
+		//adição dos opcodes de I
+		opcodesI.put("001111", "lui");
+		opcodesI.put("001000", "addi");
+		opcodesI.put("001010", "slti");
+		opcodesI.put("001100", "andi");
+		opcodesI.put("001101", "ori");
+		opcodesI.put("001110", "xori");
+		opcodesI.put("100011", "lw");
+		opcodesI.put("101011", "sw");
+		opcodesI.put("000001", "bltz");
+		opcodesI.put("000100", "beq");
+		opcodesI.put("000101", "bne");
+		opcodesI.put("001001", "addiu");
+		opcodesI.put("100000", "lb");
+		opcodesI.put("100100", "lbu");
+		opcodesI.put("101000", "sb");
+		
+		//adição do opcode de J
+		opcodesJ.put("000010", "j");
+		opcodesJ.put("000011", "jal");
+	}
+
+}
