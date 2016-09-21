4c194eea58c827f232d8c8124504d91a9b43f4fd
JNMarcos <marcos_joaos@hotmail.com>
2016-09-21 00:33:13 -0300

    Delimitando
    Adicionado opcodes de R, instruções J e JAL como constantes. Além disso,
    são constantes INICIO_INSTRUCAO = 0 (indica o início da instrução),
    FIM_INSTRUCAO = 32 (indica o fim da instrução) e FIM_OPCODE (indica o
    término do opcode).
    
    Criação da string "instrução" que contém a saída após a leitura dos
    dados em binário. Ex.: lui $1, 100
    
    Os inteiros "intervaloInicio" e "intervaloFim" servem para delimitar o
    intervalo da string "segmento", que é apenas parte da string "binário".
    Esse segmento serve para pegar as partes da string binário e verificar
    se bate com alguma instrução.
    
    EXPLICAÇÃO
    Suponha que a instrução seja em binário: 000 000 xxxxxxx 100000 (x são
    números quaisquer. Tamanho da instrução é bem menor que o usual apenas
    para explicação). String segmento = opcode do binário. Se verifca pelo
    OPCODE se é uma instrução R (000000) e no caso, constata-se que sim.
    (senão fosse verificaria se é do J, caso não fosse esse, seria I).
    Depois faz a string  segmento receber outra parte do binário, mas agora
    você sabe que é R, então você sabe que a instrução R é composta por um 3
    registradores representados em binário com 5 caracteres, logo segmento
    receberá do fim do opcode + 5, para identificar o primeiro registrador.
    Para identificar o segundo registrador, vai do fim do primeiro + 5 e
    assim vai. Faz algo semelhante aos outros  tipos de instrução (J e I).

--- src/desenvolvimento/Inicializador.java
+++ src/desenvolvimento/Inicializador.java
@@ -4,14 +4,56 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Inicializador {
-	 public static void main(String[] args) throws IOException{
-		 ArrayList<String> instrucoes;
-		 instrucoes = Leitura.lerArquivo();
-		 String binario;
-		 
-		 for(int i=0; i<instrucoes.size();i++){
+	public static final int INICIO_INSTRUCAO = 0;
+	public static final int FIM_INSTRUCAO = 32;
+	public static final int FIM_OPCODE = 6;
+	public static final String OPCODE_R = "000000"; 
+	public static final String OPCODE_J = "000010";
+	public static final String OPCODE_JAL = "000011";
+
+	public static void main(String[] args) throws IOException{
+		ArrayList<String> instrucoes;
+		instrucoes = Leitura.lerArquivo();
+		//contém a instrução em binário
+		String binario;
+		//contém a instrução em instrução humana
+		String instrucao = "";
+
+		//contém um segmento da instrução
+		String segmento;
+		int intervaloInicio;
+		int intervaloFim;
+
+		for(int i=0; i<instrucoes.size();i++){
+			intervaloInicio = INICIO_INSTRUCAO;
+			intervaloFim = FIM_OPCODE;
 			binario = Conversor.converterHexBin(instrucoes.get(i));
-			System.out.println(binario);
-		 }
-	 }
+			segmento = binario.substring(intervaloInicio, intervaloFim);
+
+			if (segmento == OPCODE_R){ // é uma instrução do tipo R
+				//FAÇA AQUI PARA IDENTIFICAR QUAL INSTRUÇÃO R É
+				//USE O SEGMENTO PARA SELECIONAR AS OUTRAS PARTES DA STRING BINÁRIO
+				// USE INTERVALOINICIO E FIM PARA ISSO (PARA ANDAR PELA STRING BINARIO
+				// E PASSAR PARA O SEGMENTO)
+				//...
+				//instrucao = instrucao + ALGUMA COISA;
+				//...
+				
+			} else if (segmento == OPCODE_J || segmento == OPCODE_JAL){ // é uma instrução do tipo J
+				//FAÇA AQUI PARA IDENTIFICAR QUAL INSTRUÇÃO J É
+				//MESMA COISA DO QUE TEM EM R
+				//...
+				//instrucao = instrucao + ALGUMA COISA;
+				//...
+			} else { // é uma instrução do tipo I
+				//FAÇA AQUI PARA IDENTIFICAR QUAL INSTRUÇÃO I É
+				//MESMA COISA DO QUE TEM EM R
+				//...
+				//instrucao = instrucao + ALGUMA COISA;
+				//...
+			}
+				System.out.println(binario);
+				System.out.println(instrucao);
+		}
+	}
 }
