e5389c98715a4a7edec08a0fea80597197730d5a
JNMarcos <marcos_joaos@hotmail.com>
2016-09-21 00:53:32 -0300

    Registradores
    Na classe Info, os registradores foram inseridos para facilitar a
    escrita da instrução compreendida facilmente por humanos. Ex.:  sub $1,
    $2, $3

--- src/desenvolvimento/Info.java
+++ src/desenvolvimento/Info.java
@@ -13,6 +13,7 @@ public class Info {
 	Hashtable<String, String> funcoesR = new Hashtable<>();
 	Hashtable<String, String> opcodesI = new Hashtable<>();
 	Hashtable<String, String> opcodesJ = new Hashtable<>();
+	Hashtable<String, String> registradores = new Hashtable<>();
 	
 	Info(){
 		//adição das funções tipos R
@@ -59,6 +60,40 @@ public class Info {
 		//adição do opcode de J
 		opcodesJ.put("000010", "j");
 		opcodesJ.put("000011", "jal");
+		
+		// adição dos registradores
+		registradores.put("00000", "$0");
+		registradores.put("00001", "$1");
+		registradores.put("00010", "$2");
+		registradores.put("00011", "$3");
+		registradores.put("00100", "$4");
+		registradores.put("00101", "$5");
+		registradores.put("00110", "$6");
+		registradores.put("00111", "$7");
+		registradores.put("01000", "$8");
+		registradores.put("01001", "$9");
+		registradores.put("01010", "$10");
+		registradores.put("01011", "$11");
+		registradores.put("01100", "$12");
+		registradores.put("01101", "$13");
+		registradores.put("01110", "$14");
+		registradores.put("01111", "$15");
+		registradores.put("10000", "$16");
+		registradores.put("10001", "$17");
+		registradores.put("10010", "$18");
+		registradores.put("10011", "$19");
+		registradores.put("10100", "$20");
+		registradores.put("10101", "$21");
+		registradores.put("10110", "$22");
+		registradores.put("10111", "$23");
+		registradores.put("11000", "$24");
+		registradores.put("11001", "$25");
+		registradores.put("11010", "$26");
+		registradores.put("11011", "$27");
+		registradores.put("11100", "$28");
+		registradores.put("11101", "$29");
+		registradores.put("11110", "$30");
+		registradores.put("11111", "$31");
 	}
 
 }
